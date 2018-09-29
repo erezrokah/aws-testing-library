48ba67d12dda8462e31120c73c085cf8051d8306
erezrokah <erezrokah@users.noreply.github.com>
2018-09-29 17:44:55 +0300

    add ci

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
+  roots: ['<rootDir>/src'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  reporters: ['default', 'jest-junit'],
 };
