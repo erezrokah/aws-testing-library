f91ed36f3ad3776716c0f678ad4204fa9e29141c
Dane Mauland <dane@improovy.com>
2022-04-07 17:50:39 -0400

    fix: not.toHaveLog no longer returns a false pass when waiting for CloudWatch to populate

--- src/jest/utils.test.ts
+++ src/jest/utils.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { ICommonProps } from '../common';
-import { wrapWithRetries } from './utils';
+import { wrapWithRetries, wrapWithRetryUntilPass } from './utils';
 
 jest.mock('../common');
 
@@ -102,6 +102,97 @@ describe('utils', () => {
       const key = 'key';
 
       const wrapped = wrapWithRetries(toWrap);
+      const result = await wrapped.bind(matcherUtils)(props, key);
+
+      expect(toWrap).toHaveBeenCalledTimes(2);
+      expect(toWrap).toHaveBeenCalledWith(props, key);
+      expect(result).toBe(expectedResult);
+      expect(sleep).toHaveBeenCalledTimes(1);
+      expect(sleep).toHaveBeenCalledWith(500); // default pollEvery
+    });
+  });
+
+  describe('wrapWithRetryUntilPass', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test('should retry once on pass === true', async () => {
+      const toWrap = jest.fn();
+      const expectedResult = { pass: true, message: () => '' };
+      toWrap.mockReturnValue(Promise.resolve(expectedResult));
+
+      const matcherUtils = {} as jest.MatcherUtils;
+
+      const props = { region: 'region' } as ICommonProps;
+      const key = 'key';
+
+      const wrapped = wrapWithRetryUntilPass(toWrap);
+      const result = await wrapped.bind(matcherUtils)(props, key);
+
+      expect(toWrap).toHaveBeenCalledTimes(1);
+      expect(toWrap).toHaveBeenCalledWith(props, key);
+      expect(result).toBe(expectedResult);
+    });
+
+    test('should exhaust timeout on pass === false', async () => {
+      const { sleep } = require('../common');
+
+      const mockedNow = jest.fn();
+      Date.now = mockedNow;
+      mockedNow.mockReturnValueOnce(0);
+      mockedNow.mockReturnValueOnce(250);
+      mockedNow.mockReturnValueOnce(500);
+      mockedNow.mockReturnValueOnce(750);
+      mockedNow.mockReturnValueOnce(1000);
+      mockedNow.mockReturnValueOnce(1250);
+
+      const toWrap = jest.fn();
+      const expectedResult = { pass: false, message: () => '' };
+      toWrap.mockReturnValue(Promise.resolve(expectedResult));
+
+      const matcherUtils = {} as jest.MatcherUtils;
+
+      const props = { timeout: 1001, pollEvery: 250 } as ICommonProps;
+      const key = 'key';
+
+      const wrapped = wrapWithRetryUntilPass(toWrap);
+      const result = await wrapped.bind(matcherUtils)(props, key);
+
+      expect(toWrap).toHaveBeenCalledTimes(5);
+      expect(toWrap).toHaveBeenCalledWith(props, key);
+      expect(result).toBe(expectedResult);
+      expect(sleep).toHaveBeenCalledTimes(4);
+      expect(sleep).toHaveBeenCalledWith(props.pollEvery);
+    });
+
+    test('should retry twice, { pass: false, isNot: false } => { pass: true, isNot: false }', async () => {
+      const { sleep } = require('../common');
+
+      const mockedNow = jest.fn();
+      Date.now = mockedNow;
+      mockedNow.mockReturnValueOnce(0);
+      mockedNow.mockReturnValueOnce(250);
+      mockedNow.mockReturnValueOnce(500);
+
+      const toWrap = jest.fn();
+      // first attempt returns pass === false
+      toWrap.mockReturnValueOnce(
+        Promise.resolve({ pass: false, message: () => '' }),
+      );
+
+      // second attempt returns pass === true
+      const expectedResult = { pass: true, message: () => '' };
+      toWrap.mockReturnValueOnce(Promise.resolve(expectedResult));
+
+      const matcherUtils = {
+        isNot: false,
+      } as jest.MatcherUtils;
+
+      const props = {} as ICommonProps;
+      const key = 'key';
+
+      const wrapped = wrapWithRetryUntilPass(toWrap);
       const result = await wrapped.bind(matcherUtils)(props, key);
 
       expect(toWrap).toHaveBeenCalledTimes(2);
