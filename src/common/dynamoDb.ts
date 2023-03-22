36a2cc7a2e497699ada007efd8bd4e851756f364
Renan Costa <renanbym@gmail.com>
2023-03-22 22:23:35 +0000

    removing dependency on filter-obj

--- src/common/dynamoDb.ts
+++ src/common/dynamoDb.ts
@@ -1,4 +1,3 @@
-import filterObject from 'filter-obj';
 import { AttributeMap } from 'aws-sdk/clients/dynamodb';
 import { ICommonProps } from './';
 
@@ -7,6 +6,29 @@ export interface IDynamoDbProps extends ICommonProps {
 }
 
 export const expectedProps = ['region', 'table', 'key'];
+
+type predicate =  (key: any, value: any, object: any) => boolean | Array<any>
+
+const filterObject = (
+  object: AttributeMap,
+  predicate: predicate,
+) => {
+  const result = {};
+  const isArray = Array.isArray(predicate);
+
+  for (const [key, value] of Object.entries(object)) {
+    if (isArray ? predicate.includes(key) : predicate(key, value, object)) {
+      Object.defineProperty(result, key, {
+        value,
+        writable: true,
+        enumerable: true,
+        configurable: true,
+      });
+    }
+  }
+
+  return result;
+};
 
 export const removeKeysFromItemForNonStrictComparison = (
   received: AttributeMap,
